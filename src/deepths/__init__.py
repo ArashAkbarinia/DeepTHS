3a7887e5c725ac8d3f10ebdcce10df3d2bff673b
Arash Akbarinia <akbarinia.arash@gmail.com>
2022-07-26 14:36:16 +0200

    colour discrimination dump

--- src/deepths/__init__.py
+++ src/deepths/__init__.py
@@ -1,3 +1,4 @@
 from .csf_train import main as csf_train_main
 from .csf_test import main as csf_test_main
 from .kernels_activation import main as activation_main
+from .colour_discrimination import main as colour_discrimination_main
