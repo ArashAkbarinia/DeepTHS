51e0aa88a35d87f0c327e8683d3ed972429d23b0
Arash Akbarinia <akbarinia.arash@gmail.com>
2022-11-30 16:52:05 +0100

    coloru cat odd4 main

--- src/deepths/__init__.py
+++ src/deepths/__init__.py
@@ -3,3 +3,4 @@ from .csf_test import main as csf_test_main
 from .kernels_activation import main as activation_main
 from .colour_discrimination import main as colour_discrimination_main
 from .odd_one_out import main as odd_one_out_main
+from .experiments.colour_categorisation_odd4 import main as colour_cat_odd4
